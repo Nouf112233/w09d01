bfd68f670607a8dad4e6b108dde89126489123a7
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-05 19:36:51 +0300

    add

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,69 +1,95 @@
-import React,{useEffect, useState} from 'react'
-import axios from 'axios'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
 
-const Tasks = ({token}) => {
-    const [taskUser,setTaskUser]=useState([]);
-    // const [taskAdmin,setTaskAdmin]=useState([]);
-    const [up,setUp]=useState(false);
-    const [toke,setToke]=useState("");
+const Tasks = ({ token }) => {
+  const [taskUser, setTaskUser] = useState([]);
+  // const [taskAdmin,setTaskAdmin]=useState([]);
+  const [up, setUp] = useState(false);
+  const [toke, setToke] = useState("");
+  const [taskName, setTaskname] = useState("");
 
-    useEffect(() => {
-        
-        const tok=localStorage.getItem("Token");
-        getTasks(tok);
-        setToke(tok);
-        // console.log("tok",tok);
-        // getTasksAdmin(tok);     
-        
-    },[])
+  useEffect(() => {
+    const tok = localStorage.getItem("Token");
+    getTasks(tok);
+    setToke(tok);
+    // console.log("tok",tok);
+    // getTasksAdmin(tok);
+  }, [up]);
 
-    const getTasks=async(tok)=>{
-       try{
-        const tasks=await axios.get(`${process.env.REACT_APP_BASIC_URL}/tasks`, {headers:{Authorization:`Bearer ${toke}`}})
-        console.log("tasks",tasks.data);
-        setTaskUser(tasks.data);
-        // setRole("user");
-       }catch(error) {
-           console.log(error);
-       }
+  const getTasks = async (tok) => {
+    try {
+      const tasks = await axios.get(
+        `${process.env.REACT_APP_BASIC_URL}/tasks`,
+        { headers: { Authorization: `Bearer ${tok}` } }
+      );
+    //   console.log("tasks", tasks.data);
+      setTaskUser(tasks.data);
+      // setRole("user");
+    } catch (error) {
+      console.log(error);
     }
-    // const getTasksAdmin=async(tok)=>{
-    //     try{
-    //      const tasks=await axios.get(`${process.env.REACT_APP_BASIC_URL}/alltasks`, {headers:{Authorization:`Bearer ${tok}`}})
-    //      console.log("tasksadmin",tasks.data);
-    //      setTaskAdmin(tasks.data);
-    //      setRole("admin");
-    //     }catch(error) {
-    //         console.log(error);
-    //     }
-    //  }
+  };
+  // const getTasksAdmin=async(tok)=>{
+  //     try{
+  //      const tasks=await axios.get(`${process.env.REACT_APP_BASIC_URL}/alltasks`, {headers:{Authorization:`Bearer ${tok}`}})
+  //      console.log("tasksadmin",tasks.data);
+  //      setTaskAdmin(tasks.data);
+  //      setRole("admin");
+  //     }catch(error) {
+  //         console.log(error);
+  //     }
+  //  }
 
-    const deletetask=(_id)=>{
-        axios.delete(`${process.env.REACT_APP_BASIC_URL}/task`,{taskId:_id}, {headers:{Authorization:`Bearer ${tok}`}})
-        setUp(!up);
-    }
+  const deletetask = async(taskId) => {
+     
+     try{
+         const result=await axios.delete(
+        `${process.env.REACT_APP_BASIC_URL}/task`,
+        {data:{taskId}},
+        { headers: { Authorization: `Bearer ${toke}` } }
+      );
+    setUp(!up);
+     }catch(error){
+        console.log(error);
+     }
 
-    return (
-        <div>
-           
-           { taskUser.length &&
-           taskUser.map(item=>{
-               console.log("item",item)
-               return <div>
-                   <h1>{item.name}</h1>
-                   <button onClick={()=>deletetask(item._id)}>delete</button>
-                   <button onClick={()=>updatetask(item._id)}>update</button>
-                   </div>
-               
-      
-
-           })
-           
-           }
-          
-        </div>
-    )
 }
 
-export default Tasks
+  const updatetask = (_id) => {
+      if(taskName.length>0){
+    axios.put(
+      `${process.env.REACT_APP_BASIC_URL}/task`,
+      { taskId: _id,taskName:taskName },
+      { headers: { Authorization: `Bearer ${toke}` } }
+    );
+      }
+    setTaskname("");
+    setUp(!up);
+  };
+
+  return (
+    <div>
+      {taskUser.length &&
+        taskUser.map((item) => {
+          console.log("item", item);
+          return (
+            <div>
+              <h1>{item.name}</h1>
+              <button onClick={() => deletetask(item._id)}>delete</button>
+              <input
+                type="text"
+                value={taskName}
+                onChange={(e) => {
+                  setTaskname(e.target.value);
+                }}
+              />
+              <button onClick={() => updatetask(item._id)}>update</button>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Tasks;
